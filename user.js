c6a2d87a7ff4dd66486f9494a8fefdc2641e3e9b
Joabe <joabe.de.silva@gmail.com>
2023-06-08 18:53:09 -0300

    criando arquivos html e 2 js para trabalhar com modules

---
+++ user.js
@@ -0,0 +1,15 @@
+class User {
+    constructor(name, age) {
+        this.name = name
+        this.age = age 
+    }
+    
+}
+
+function printName(user) {
+    console.log(`User's name is ${user.name}.`)
+}
+
+function printAge(user) {
+    console.log(`User's age is ${user.age} years old.`)
+}
